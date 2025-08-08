f6b3a372044d8612bd85f6a1e131eff0e8e61be2
huy-nguyen190603 <101305249+huy-nguyen190603@users.noreply.github.com>
2025-08-08 23:47:38 +0000

    remove padding

--- src/components/WorkSection.tsx
+++ src/components/WorkSection.tsx
@@ -80,7 +80,7 @@ const WorkSection = () => {
     link: "#",
     canvaLink: "https://www.canva.com/design/DAGths7jopE/BmKMiqVTFU8B63FBnuTlxg/edit?utm_content=DAGths7jopE&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
   }];
-  return <section className="px-6 py-[60px]">
+  return <section className="px-6">
       <div className="max-w-7xl mx-auto">
         <div className="mb-16">
           <h2 className="text-2xl md:text-3xl font-inter font-bold tracking-tight mb-6 text-center">Selected UX Projects</h2>
