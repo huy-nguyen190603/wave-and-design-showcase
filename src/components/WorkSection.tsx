41020e42611574250b0e263cf9c945e2361116f1
huy-nguyen190603 <101305249+huy-nguyen190603@users.noreply.github.com>
2025-08-09 09:31:35 +0000

    delete one div

--- src/components/WorkSection.tsx
+++ src/components/WorkSection.tsx
@@ -202,7 +202,7 @@ const WorkSection = () => {
                 
                 {/* SNAPCARE Button at Bottom */}
                 {project.id === 2 && <div className="px-8 pb-8">
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+                    {/*<div className="grid grid-cols-1 md:grid-cols-2 gap-3">*/}
                       {/* Empty div to align with second column */}
                       <div className="flex justify-end">
                         <Button onClick={() => window.open('https://www.figma.com/proto/e71vytYcbY7rb760X9rvhU/Snapcare--Official-?page-id=0%3A1&node-id=439-5329&p=f&viewport=-1404%2C1316%2C0.26&t=Dt0YvonDz9FqjRZk-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=437%3A4608&show-proto-sidebar=1', '_blank')} className="text-xs flex items-center gap-2 hover:opacity-90 transition-opacity py-2 px-4 rounded-full bg-primary text-primary-foreground">
@@ -210,7 +210,7 @@ const WorkSection = () => {
                           Open Prototype in Figma
                         </Button>
                       </div>
-                    </div>
+                    {/*</div>*/}
                   </div>}
               </CardContent>
             </Card>
