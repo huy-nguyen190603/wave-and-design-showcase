dcaaced7500eb2c443111b97d1a578b845ac06a2
huy-nguyen190603 <101305249+huy-nguyen190603@users.noreply.github.com>
2025-08-17 19:48:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/wave-and-design-showcase/',
   server: {
     host: "::",
     port: 8080,
